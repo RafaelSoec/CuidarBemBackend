0a116cb42e76bb42a7ae226af9888830b94642f3
Rafael Souza <rafaelsoec@gmail.com>
2021-11-07 22:23:20 -0300

    Varias mudanças

--- src/main/java/com/crescer/v1/service/EmailService.java
+++ src/main/java/com/crescer/v1/service/EmailService.java
@@ -55,7 +55,7 @@ public class EmailService {
 			String mensagem = criarMensagemEmailPorHtml(pathHTML, variaveis);
 			this.enviarHtmlEmail(email, mensagem);
 		} catch (Exception e) {
-			throw new ResponseException(e.getMessage());
+			throw new ResponseException("Falha ao enviar HTML por email. ");
 
 		}
 	}
@@ -72,7 +72,7 @@ public class EmailService {
 			mmh.setFrom(user);
 			javaMailSender.send(mimeMessage);
 		} catch (MessagingException e) {
-			throw new ResponseException(e.getMessage());
+			throw new ResponseException("Falha ao enviar HTML por email. ");
 
 		}
 	}
